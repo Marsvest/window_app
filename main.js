f9115ebbb9f90832e8feecc9bfa8f3097c03ff7b
mrart <bykov.ad@students.dvfu.ru>
2023-12-10 10:19:10 +1000

    component add

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-var block = new Vue({
-    el: '#block',
-    data: {
-        x: 0,
-        y: 0,
-        color: 'rgb(255,255,255)'
+Vue.component('comp', {
+    template: `
+    <div id="block" :style="{ left: x + '%', bottom: y + '%', backgroundColor: color }">
+    </div>
+    `,
+    data() {
+        return {
+            x: 0,
+            y: 0,
+            color: 'rgb(255,255,255)',
+            intervalId: null,
+        };
     },
     created() {
-        this.spawn()
+        this.spawn();
         this.intervalId = setInterval(this.move, 50);
     },
     methods: {
@@ -24,11 +30,18 @@ var block = new Vue({
         },
         spawn() {
             this.x = this.generateRandomPos();
-            this.y = 100
+            this.y = 100;
             this.color = this.generateRandomColor();
         },
         move() {
             if (this.y > 0) this.y -= 0.5;
-        }
-    }
+        },
+    },
+    beforeDestroy() {
+        clearInterval(this.intervalId);
+    },
 });
+
+var app = new Vue({
+    el: '#app'
+})
