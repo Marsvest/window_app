626d88d63c5d56d61ba142bcbdc3f70b083a7423
mrart <bykov.ad@students.dvfu.ru>
2023-12-17 14:55:05 +1000

    x pos collision added

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 Vue.component('block', {
     props: {
-        pos_x: Number, pos_y: Number, color1: String
+        pos_x: Number, pos_y: Number, color1: String, index1: Number
     }
     ,
     template: `
@@ -11,7 +11,8 @@ Vue.component('block', {
         return {
             x: this.pos_x,
             y: this.pos_y,
-            color: this.color1
+            color: this.color1,
+            index: this.index1
         };
     },
     created() {
@@ -19,7 +20,11 @@ Vue.component('block', {
     },
     methods: {
         move() {
-            if (this.y > 0) this.y -= 0.5;
+            if (this.index === 0 && this.y > 0) {
+                this.y -= 0.5;
+            } else if (this.index > 0 && this.y > this.$parent.positions_y[this.index - 1] + 10) {
+                this.y -= 0.5;
+            }
         }
     }
 });
@@ -27,14 +32,27 @@ Vue.component('block', {
 var app = new Vue({
     el: '#app',
     data: {
-        items: []
+        index_counter: 0,
+        items: [],
+        positions_y: [],
+        positions_x: []
     },
     created() {
-        setInterval(this.add_block, 2000);
+        setInterval(this.add_block, 5000);
+        setInterval(this.accessBlockY, 50);
     },
     methods: {
         add_block() {
-            this.items.push({ pos_x: this.generateRandomPos(), pos_y: 100, color1: this.generateRandomColor() })
+            this.items.push({ pos_x: this.generateRandomPos(), pos_y: 100, color1: this.generateRandomColor(), index1: this.index_counter })
+            this.index_counter++;
+        },
+        accessBlockY() {
+            for (let i = 0; i < this.items.length; i++) {
+                const currentBlockY = this.$refs.myBlock[i].y;
+                const currentBlockX = this.$refs.myBlock[i].x;
+                this.positions_y[i] = currentBlockY;
+                this.positions_x[i] = currentBlockX;
+            }
         },
         generateRandomColor() {
             const randomRed = Math.floor(Math.random() * 256);
